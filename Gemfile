4488602be272e9bc7fddd494c22dc22ef68481e0
Yohan <yohanfernando@gmail.com>
2014-08-13 22:02:22 +0000

    WIP Adding a bit of styling using bootstrap

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'pg'
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 group :development do
   gem 'spring'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
   
   def after_sign_in_path_for(resource)
     search_path
+  end
+
+  protected
+
+  def layout_by_resource
+    puts params[:controller]
+    puts params[:action]    
+    controller_action = "#{params[:controller]}/#{params[:action]}"
+
+    case controller_action
+    when "devise/passwords/new", "devise/passwords/create", 
+      "devise/sessions/new", "devise/sessions/create",
+      "devise/registrations/new", "devise/registrations/create"
+      "no_layout"
+    else
+      "application"
+    end
   end
 end
