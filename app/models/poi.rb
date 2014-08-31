fcf4e634743d3d3a774093b16c00eea7cdb56112
Yohan <yohanfernando@gmail.com>
2014-08-31 19:48:04 +0000

    Display the my poi list with the map

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,3 +1,8 @@
 class Poi < ActiveRecord::Base
   belongs_to :category
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :category_id, presence: true
+  validates :name, presence: true
 end
