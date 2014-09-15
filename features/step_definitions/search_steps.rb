1bf3ee4302879d63cbd014d1e7580c8d623c9ed2
Yohan <yohanfernando@gmail.com>
2014-09-15 03:02:22 +0000

    Finishing tests and beautify map icons

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -5,3 +5,11 @@ end
 Then(/^I should be redirected to the login page$/) do                                                              
   expect(current_path).to eq new_user_session_path
 end
+
+When(/^I search for Pois$/) do
+  select('Bridge', :from => 'category_id')
+end
+
+Then(/^I should be displayed a map with the Pois$/) do
+  expect(page).to have_css('#map-canvas')
+end
